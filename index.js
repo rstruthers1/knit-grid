32c7fe75eb0d62a9e8acd8ccd577729bcd49edf5
Rachel Struthers <rachel.struthers@sap.com>
2018-12-20 15:09:01 -0600

    Trying out mysql

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
-const path = require('path');
+const path = require('path')
+const mysql = require('mysql')
+require('dotenv').config();
+
+
+
 
 const app = express();
 // Serve static files from the React app
@@ -22,7 +27,26 @@ app.get('/api/greeting', (req, res) => {
 app.post('/api/saveKnitData', (req, res) => {
   console.log("***** Data: " + JSON.stringify(req.body))
   res.setHeader('Content-Type', 'application/json')
-  res.send(JSON.stringify({message: "success"}))
+  // res.send(JSON.stringify({message: "success"}))
+
+  const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      res.send(JSON.stringify({message: "error connecting to database"}))
+      return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+    res.send(JSON.stringify({message: "success", connectionId: connection.threadId}))
+
+  });
+
+  connection.end();
+
+
+
+
 })
 
 // The "catchall" handler: for any request that doesn't
