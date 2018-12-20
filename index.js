0ada2a14e37b875cc5f84f19c52d0a051508b319
Rachel Struthers <rachel.struthers@sap.com>
2018-12-20 09:47:50 -0600

    Fix to serve up react files

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const path = require('path');
 
 const app = express();
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//app.use(pino);
+app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
@@ -20,6 +24,12 @@ app.post('/api/saveKnitData', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   res.send(JSON.stringify({message: "success"}))
 })
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 
